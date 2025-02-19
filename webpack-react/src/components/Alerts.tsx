39f17adda08b34e2dd4f6c596fccf0a3efce7866
Ivan Grechkan <jarvis.zoorgut@gmail.com>
2025-02-19 21:32:31 +0500

    F1 final - 19/02/2025

--- webpack-react/src/components/Alerts.tsx
+++ webpack-react/src/components/Alerts.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import * as React from "react";
 import Alert from "react-bootstrap/Alert";
 
 
 function Alerts(props) {
+    let children = props.children;
     return (
         <>
-            { props.clildren }
+            { 
+                React.Children.map(children, (child, index) => {
+                    if(index < 2) {
+                        return child;
+                    }
+                })
+            }
+            {
+                React.Children.count(props.children)
+            }
         </>
     );
 }

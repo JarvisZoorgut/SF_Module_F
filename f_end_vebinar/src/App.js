6fb419b91ac07d603141593d7525ad1c34eb0f1e
Ivan Grechkan <jarvis.zoorgut@gmail.com>
2025-03-21 16:50:52 +0500

    f_end_vebinar

--- f_end_vebinar/src/App.js
+++ f_end_vebinar/src/App.js
@@ -4,13 +4,16 @@ import { Students } from "./components/students";
 import "./styles.css";
 
 export default function App() {
-
+  const [mainList, setMainList] = React.useState([]);
 
   return (
     <div className="App">
       <Students label="Поток 1" />
       <Students label="Поток 2" />
       <Students label="Поток 3" />
+      <br />
+
+
     </div>
   );
 }

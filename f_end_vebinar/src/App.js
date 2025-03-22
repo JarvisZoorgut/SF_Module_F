34efa3d57e3898fa9867c269ac67efa04219e01d
Ivan <jarvis.zoorgut@gmail.com>
2025-03-23 02:11:20 +0500

    F - 23/03/2025 - final f_end_vebinar

--- f_end_vebinar/src/App.js
+++ f_end_vebinar/src/App.js
@@ -6,12 +6,23 @@ import "./styles.css";
 export default function App() {
   const [mainList, setMainList] = React.useState([]);
 
+  const onMoveStudents = (arr) => {
+    setMainList([...mainList, ...arr])
+  }
+
   return (
     <div className="App">
-      <Students label="Поток 1" />
-      <Students label="Поток 2" />
-      <Students label="Поток 3" />
-
+      <Students label="Поток 1" onMove={onMoveStudents} />
+      <Students label="Поток 2" onMove={onMoveStudents} />
+      <Students label="Поток 3" onMove={onMoveStudents} />
+      <br />
+      <hr />
+      <h2>Все студенты:</h2>
+      <ul>
+        {mainList.map((name) => (
+        <li>{name}</li>
+        ))}
+      </ul>
     </div>
   );
 }

db55902e70beae8da171c0725a8de00a01ec4c81
Ivan <jarvis.zoorgut@gmail.com>
2025-03-23 17:46:36 +0500

    F6 - 23/03/2025 - vebinar 2 finish

---
+++ f_end_vebinar_2/app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div className="footer">
+            <a href="https://vk.com/grechkan" target="_blank" rel="noopener noreferrer">Vk</a>
+            <a href="https://github.com/JarvisZoorgut" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </div>
+    )
+}
---
+++ f_end_vebinar_2/app/src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Main = () => {
+    return (
+        <div className="main">
+            <img className= 'img' src="https://moya-planeta.ru/upload/images/xl/85/ec/85ec639804ea05eb0d5bf4e6793c540e5951d508.jpg" alt="mountain" />
+            <p className="par">Горы горы горы</p>
+        </div>
+    )
+}
